2ed43866fd59500f843d58a99721bb58368c9c31
Todd Lang <todd.lang@kiyote.ca>
2022-10-06 11:58:12 -0400

    Adding AWS policy to role (#10)
    * Adding AWS policy to role
    
    * Fix role reference
    
    Co-authored-by: Todd Lang <todd.lang@d2l.com>

--- terraform/modules/amplify/role.tf
+++ terraform/modules/amplify/role.tf
@@ -12,8 +12,19 @@ data "aws_iam_policy_document" "amplify_assume_role_policy_document" {
     }
 }
 
+data "aws_iam_policy_document" "app_policy_document" {
+    statement {
+        effect = "Allow"
+    }
+}
+
 resource "aws_iam_role" "app_role" {
-    name = "${var.object_prefix}.app_role"
+    name = "${var.object_prefix}app_role"
 
     assume_role_policy = data.aws_iam_policy_document.amplify_assume_role_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "amplify_administrator" {
+    role = aws_iam_role.app_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-Amplify"
 }

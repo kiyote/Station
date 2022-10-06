5385ed2be238589488d780890d4c91930a1bac95
Todd Lang <todd.lang@d2l.com>
2022-10-06 10:03:31 -0400

    Tweaking the app settings

--- terraform/modules/amplify/app.tf
+++ terraform/modules/amplify/app.tf
@@ -6,21 +6,27 @@ resource "aws_amplify_app" "station" {
     build_spec = <<-EOT
         version: 1
         frontend:
-        phases:
-        preBuild:
-        commands:
-        - curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-        - chmod +x *.sh
-        - ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
-        - ./dotnet6/dotnet --version
-        build:
-        commands:
-        - ./dotnet6/dotnet publish -c Release -o releas
-        artifacts:
-        baseDirectory: /release/wwwroot
-        files:
-        - '**/*'
-        cache:
-        paths: []
+          phases:
+            preBuild:
+              commands:
+                - curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+                - chmod +x *.sh
+                - ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
+                - ./dotnet6/dotnet --version
+            build:
+              commands:
+                - ./dotnet6/dotnet publish -c Release -o releas
+          artifacts:
+            baseDirectory: /release/wwwroot
+            files:
+              - '**/*'
+          cache:
+            paths: []
     EOT
+
+    custom_rule {
+        source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json|br|gz|html|md|eot|otf|dll|blat|wasm|dat)$)([^.]+$)/>"
+        status = "200"
+        target = "/index.html"
+    }
 }

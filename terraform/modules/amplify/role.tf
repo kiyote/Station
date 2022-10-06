290c2daca27cdc1cb347394d9a536d80322d4105
Todd Lang <todd.lang@d2l.com>
2022-10-06 11:36:29 -0400

    Fixing document reference

--- terraform/modules/amplify/role.tf
+++ terraform/modules/amplify/role.tf
@@ -15,5 +15,5 @@ data "aws_iam_policy_document" "amplify_assume_role_policy_document" {
 resource "aws_iam_role" "app_role" {
     name = "${var.object_prefix}.app_role"
 
-    assume_role_policy = data.aws_iam_policy_document.json
+    assume_role_policy = data.aws_iam_policy_document.amplify_assume_role_policy_document.json
 }

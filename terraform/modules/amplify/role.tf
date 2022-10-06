9638387dc74f14e192f0c534cec2c8b212b239e3
Todd Lang <todd.lang@kiyote.ca>
2022-10-06 12:01:26 -0400

    Removing mistaken resource (#11)
    Co-authored-by: Todd Lang <todd.lang@d2l.com>

--- terraform/modules/amplify/role.tf
+++ terraform/modules/amplify/role.tf
@@ -12,12 +12,6 @@ data "aws_iam_policy_document" "amplify_assume_role_policy_document" {
     }
 }
 
-data "aws_iam_policy_document" "app_policy_document" {
-    statement {
-        effect = "Allow"
-    }
-}
-
 resource "aws_iam_role" "app_role" {
     name = "${var.object_prefix}app_role"
 

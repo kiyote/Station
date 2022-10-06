d04d39918c14fba824c165d58f97fd8d712b57b1
Todd Lang <todd.lang@d2l.com>
2022-10-06 13:32:17 -0400

    Reformatting the yaml

--- terraform/modules/amplify/app.tf
+++ terraform/modules/amplify/app.tf
@@ -7,22 +7,22 @@ resource "aws_amplify_app" "station" {
     build_spec = <<-EOT
 version: 1
 frontend:
-phases:
-preBuild:
-    commands:
-    - curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-    - chmod +x *.sh
-    - ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
-    - ./dotnet6/dotnet --version
-build:
-    commands:
-    - ./dotnet6/dotnet publish -c Release -o releas
-artifacts:
-baseDirectory: /release/wwwroot
-files:
-    - '**/*'
-cache:
-paths: []
+  phases:
+    preBuild:
+      commands:
+        - curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+        - chmod +x *.sh
+        - ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
+        - ./dotnet6/dotnet --version
+    build:
+      commands:
+        - ./dotnet6/dotnet publish -c Release -o releas
+  artifacts:
+    baseDirectory: /release/wwwroot
+    files:
+      - '**/*'
+  cache:
+    paths: []
     EOT
 
     custom_rule {

3c4a1d95e184207b979f1017e4d62971db99b462
Todd Lang <todd.lang@d2l.com>
2022-10-15 14:29:59 -0400

    Trying another approach for output variable

--- terraform/environments/dev/infrastructure.tf
+++ terraform/environments/dev/infrastructure.tf
@@ -7,9 +7,13 @@ module "station" {
 }
 
 variable "repo_token" {
-  description = "Carries the access token for the repository."
+  description = "Carries the access token for the repository. This value comes from TF_VAR_repo_token in the GitHub Environment."
   type = string
   default = ""
   sensitive = true
   nullable = false
+}
+
+output "build_webhook" {
+  value = module.amplify.amplify_webhook_url
 }

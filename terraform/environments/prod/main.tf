5e259fe891408c26f1602aa0d25eee76a109926f
Todd Lang <todd.lang@d2l.com>
2022-10-16 09:53:34 -0400

    Fixing module path reference

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "webclient" {
-    source = "../../webclient"
+    source = "../../modules/webclient"
 
     object_prefix = var.object_prefix
     repo_token = var.repo_token

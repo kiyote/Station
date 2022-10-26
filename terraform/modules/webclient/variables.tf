229bcaa4303507889926b26b4a724e8af4098fae
Todd Lang <todd.lang@d2l.com>
2022-10-26 10:50:43 -0400

    Removing variables from webclient module

--- terraform/modules/webclient/variables.tf
+++ terraform/modules/webclient/variables.tf
@@ -1,19 +1,3 @@
 variable "object_prefix" {
   type = string
 }
-
-variable "repo_token" {
-  type = string
-}
-
-variable "webclient_stage" {
-  type = string
-}
-
-variable "source_repository" {
-  type = string
-}
-
-variable "source_repository_branch" {
-  type = string
-}

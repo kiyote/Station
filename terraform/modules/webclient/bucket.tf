e7fe2a400747f254f02786bcf70e32d112a3a047
Todd Lang <todd.lang@d2l.com>
2022-10-26 15:22:11 -0400

    Twiddling OAC

--- terraform/modules/webclient/bucket.tf
+++ terraform/modules/webclient/bucket.tf
@@ -47,7 +47,7 @@ data "aws_iam_policy_document" "bucket_policy" {
     principals {
       type = "AWS"
       identifiers = [
-        "${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"
+        "${aws_cloudfront_origin_access_control.distribution_access.arn}"
       ]
     }
   }

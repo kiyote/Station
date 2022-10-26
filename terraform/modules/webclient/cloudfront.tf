208a781325bcda363d162efb1b85baea8afa69de
Todd Lang <todd.lang@d2l.com>
2022-10-26 15:07:57 -0400

    More cloudfront tweaking

--- terraform/modules/webclient/cloudfront.tf
+++ terraform/modules/webclient/cloudfront.tf
@@ -1,9 +1,7 @@
 
-locals {
-  s3_origin_id = "webclient_s3_origin"
-}
 
 resource "aws_cloudfront_origin_access_control" "distribution_access" {
+    name = "webclient_access_control"
     origin_access_control_origin_type = "s3"
     signing_behavior = "always"
     signing_protocol = "sigv4"
@@ -13,7 +11,7 @@ resource "aws_cloudfront_distribution" "distribution_webclient" {
   origin {
     domain_name = aws_s3_bucket.bucket_webclient.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.distribution_access.id
-    origin_id = locals.s3_origin_id
+    origin_id = "S3-${aws_s3_bucket.bucket_webclient.bucket}"
   }
 
   enabled = true

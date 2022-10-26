2fcfea01ad7d5c82f5d10d2702707b4d74f5ee6e
Todd Lang <todd.lang@d2l.com>
2022-10-26 11:37:18 -0400

    More blocking public access stuff

--- terraform/modules/webclient/bucket.tf
+++ terraform/modules/webclient/bucket.tf
@@ -19,3 +19,12 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
     bucket = aws_s3_bucket.bucket_webclient.bucket
     acl = "private"
 }
+
+resource "aws_s3_public_access_block" "bucket_public" {
+    bucket = aws_s3_bucket.bucket_webclient.bucket
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}

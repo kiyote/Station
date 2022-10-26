2431a8458c1b7341f174cc04e214b0112217d8db
Todd Lang <todd.lang@d2l.com>
2022-10-26 12:06:43 -0400

    Adding missing bucket reference

--- terraform/modules/webclient/bucket.tf
+++ terraform/modules/webclient/bucket.tf
@@ -1,34 +1,36 @@
 
 resource "aws_s3_bucket" "bucket_webclient" {
-    bucket = "${var.object_prefix}webclient"
+   bucket = "${var.object_prefix}webclient"
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket_webclient.bucket
+
   versioning_configuration {
     status = "Disabled"
   }
 }
 
 resource "aws_s3_bucket_website_configuration" "bucket_website" {
-    bucket = aws_s3_bucket.bucket_webclient.bucket
+  bucket = aws_s3_bucket.bucket_webclient.bucket
 
-    index_document {
-        suffix = "index.html"
-    }
+  index_document {
+    suffix = "index.html"
+  }
 }
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
-    bucket = aws_s3_bucket.bucket_webclient.bucket
-    acl = "private"
+  bucket = aws_s3_bucket.bucket_webclient.bucket
+  acl = "private"
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket_public" {
-    bucket = aws_s3_bucket.bucket_webclient.bucket
+  bucket = aws_s3_bucket.bucket_webclient.bucket
 
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
 # The policy applied to the storage bucket

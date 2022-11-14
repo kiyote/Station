ef4b1f6b7615c74100dbea1f68df641b49759d6a
Todd Lang <todd.lang@d2l.com>
2022-11-14 10:55:49 -0500

    Creating container repository

---
+++ terraform/modules/server/ecr.tf
@@ -0,0 +1,32 @@
+resource "aws_ecr_repository" "repository" {
+  name                 = local.component_name
+  image_tag_mutability = "IMMUTABLE"
+}
+
+data "aws_iam_policy_document" "repository" {
+  statement {
+    sid    = "push"
+    effect = "Allow"
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetAuthorizationToken",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload"      
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = var.container_publishing_role
+    }
+  }
+}
+
+resource "aws_ecr_repository_policy" "repository" {
+  repository = aws_ecr_repository.repository.name
+  policy     = data.aws_iam_policy_document.repository.json
+}
--- terraform/modules/server/variables.tf
+++ terraform/modules/server/variables.tf
@@ -1,4 +1,11 @@
 
+data "aws_region" "current" {}
+
 variable "object_prefix" {
+  type = string
+}
+
+variable "container_publishing_role" {
+  description = "The role to allow permission to push the container to the repository."
   type = string
 }
